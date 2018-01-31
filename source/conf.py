f058da7809f15461377393468d711a0ad1974134
Guillem Marpons Ucero <gmarpons@tutanota.com>
2018-01-31 14:14:57 +0100

    Sphinx config: activar algunes extensions

--- source/conf.py
+++ source/conf.py
@@ -31,9 +31,19 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo',
+extensions = [
+    'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.extlinks',
+    'sphinxcontrib.needs',
+]
+
+extlinks = {
+    'joinup': ('https://joinup.ec.europa.eu/%s', 'joinup'),
+    'issue': ('https://github.com/sphinx-doc/sphinx/issues/%s', 'issue '),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
