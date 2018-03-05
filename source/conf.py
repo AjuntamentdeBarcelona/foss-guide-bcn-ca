339db817251f77af68bd7e95bbaa1a01773162af
Guillem Marpons Ucero <gmarpons@tutanota.com>
2018-03-05 01:24:30 +0100

    Sphinx config: deixar config per defecte a html_theme_options

--- source/conf.py
+++ source/conf.py
@@ -132,15 +132,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    'collapse_navigation': False,
-    'sticky_navigation': False,
-    'navigation_depth': 4,
-    # 'includehidden': True,
-    # 'logo_only':
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
